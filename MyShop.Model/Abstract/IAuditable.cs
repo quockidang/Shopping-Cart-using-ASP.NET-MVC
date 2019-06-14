1da28947e391f6289becdbfb4f41da4e5b4b95ea
KiDQ <quockidang@gmail.com>
2019-06-14 17:33:39 +0700

    Create domain entities for project using Entity Framework CodeFist.

---
+++ MyShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdateDate { set; get; }
+        string UpdateBy { set; get; }
+
+        string MetaKeyWord { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
