5f8f166b246f394f6ab2f83109f48f7b8977141c
KiDQ <quockidang@gmail.com>
2019-06-14 18:20:42 +0700

    add entities framework for data

---
+++ MyShop.Data/Infrastructer/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Data.Infrastructer
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private MyShopDbContext dbContext;
+        public MyShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new MyShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
